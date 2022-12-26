a0d8087a6962852dfb589833cb99f5746e726edf
Ename45 <enamesit@gmail.com>
2022-12-26 14:04:20 +0100

    correction in CalculatingSales

--- src/ChristmasSnack/CalculatingSales.java
+++ src/ChristmasSnack/CalculatingSales.java
@@ -13,12 +13,12 @@ public class CalculatingSales {
             System.out.print("Enter product number (1-5) or 0 to quit: ");
             productNum = input.nextInt();
 
+            System.out.print("Enter quantity sold: ");
+            quantity = input.nextInt();
+
             if (productNum == 0) {
                 break;
             }
-
-            System.out.print("Enter quantity sold: ");
-            quantity = input.nextInt();
 
             switch (productNum) {
                 case 1:
@@ -43,5 +43,49 @@ public class CalculatingSales {
         }
 
         System.out.printf("Total retail value of all products sold: $%.2f", total);
+
+
+
+
+
+
+//        int productNum;
+//        int quantity;
+//        double total = 0;
+//
+//        while (true) {
+//            System.out.print("Enter product number (1-5) or 0 to quit: ");
+//            productNum = input.nextInt();
+//
+//            System.out.print("Enter quantity sold: ");
+//            quantity = input.nextInt();
+//
+//            if (productNum == 0) {
+//                break;
+//            }
+//
+//            switch (productNum) {
+//                case 1:
+//                    total += 2.98 * quantity;
+//                    break;
+//                case 2:
+//                    total += 4.50 * quantity;
+//                    break;
+//                case 3:
+//                    total += 9.98 * quantity;
+//                    break;
+//                case 4:
+//                    total += 4.49 * quantity;
+//                    break;
+//                case 5:
+//                    total += 6.87 * quantity;
+//                    break;
+//                default:
+//                    System.out.println("Invalid product number.");
+//                    break;
+//            }
+//        }
+//
+//        System.out.printf("Total retail value of all products sold: $%.2f", total);
     }
 }
